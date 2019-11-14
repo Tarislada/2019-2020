70b5cef08bbd78c2b55050efa1b9f96048b61d27
Tarislada <57513585+Tarislada@users.noreply.github.com>
2019-11-14 21:55:44 +0900

    changed variable names
    beta -> alpha, alpha -> beta,

--- 2019-2advlearnpsy/MacKintosh.py
+++ 2019-2advlearnpsy/MacKintosh.py
@@ -13,10 +13,10 @@ Aprob = .5         # Probability of A trial occurrence
 Ashock = .8        # Probability of shock in A trials
 Bshock = .8         # Probability of shock in B trials
 
-alphaA = .3         # Saliency of X (CS)
-alphaB = .3         # Saliency of A (BCK)
-initbetaA = .1      # initial Learning rate of US
-initbetaB = .1      # initial Learning rate of no US
+betaA = .3         # betaency of X (CS)
+betaB = .3         # betaency of A (BCK)
+initalphaA = .1      # initial Learning rate of US
+initalphaB = .1      # initial Learning rate of no US
 bias = -0.01
 
 
@@ -24,8 +24,8 @@ REP_NUM = 100       # of experiments
 
 REP_A = np.zeros((0,trialn))
 REP_B = np.zeros((0,trialn))
-REP_betaA = np.zeros((0,trialn))
-REP_betaB = np.zeros((0,trialn))
+REP_alphaA = np.zeros((0,trialn))
+REP_alphaB = np.zeros((0,trialn))
 
 
 for rep in range(REP_NUM):
@@ -34,17 +34,17 @@ for rep in range(REP_NUM):
     VB = np.zeros(trialn)
     VA = np.zeros(trialn)
 
-    betaA = np.ones(trialn) * initbetaA  # Learning rate of US
-    betaB = np.ones(trialn) * initbetaB  # Learning rate of no US
+    alphaA = np.ones(trialn) * initalphaA  # Learning rate of US
+    alphaB = np.ones(trialn) * initalphaB  # Learning rate of no US
     for i in range(pretrain):
-        dbetaA = -1 * privatesign(abs(0 - VA[i]) - abs(0 - VB[i])) * abs(abs(0 - VA[i]) - abs(0 - VB[i]) + bias) * betaA[i]
-        dbetaB = 0
-        betaA[i + 1] = dbetaA + betaA[i]
-        betaA[i + 1] = clamp(betaA[i + 1], 0, 1)
-        betaB[i + 1] = dbetaB + betaB[i]
-        betaB[i + 1] = clamp(betaB[i + 1], 0, 1)
-
-        dA = alphaA * betaA[i] * (0 - VA[i])
+        dalphaA = -1 * privatesign(abs(0 - VA[i]) - abs(0 - VB[i])) * abs(abs(0 - VA[i]) - abs(0 - VB[i]) + bias) * (1-alphaA[i])
+        dalphaB = 0
+        alphaA[i + 1] = dalphaA + alphaA[i]
+        alphaA[i + 1] = clamp(alphaA[i + 1], 0, 1)
+        alphaB[i + 1] = dalphaB + alphaB[i]
+        alphaB[i + 1] = clamp(alphaB[i + 1], 0, 1)
+(1-)
+        dA = betaA * alphaA[i] * (0 - VA[i])
         dB = 0
         VA[i + 1] = dA + VA[i]
         VB[i + 1] = dB + VB[i]
@@ -53,27 +53,27 @@ for rep in range(REP_NUM):
     for ii in range(pretrain, trialn - 1):
         if trialorder[ii] <= Aprob * trialn:  # A trials
             if trialorder[ii] <= trialn * Aprob * Ashock:  # A, Yes shock trials
-                dbetaA = -1 * privatesign(abs(1 - VA[ii])-abs(1 - VB[ii]))* abs(abs(1 - VA[ii])-abs(1 - VB[ii])+ bias) *betaA[ii]
-                dbetaB = 0
-                betaA[ii + 1] = dbetaA + betaA[ii]
-                betaA[ii + 1] = clamp(betaA[ii + 1], 0, 1)
-                betaB[ii + 1] = dbetaB + betaB[ii]
-                betaB[ii + 1] = clamp(betaB[ii + 1], 0, 1)
+                dalphaA = -1 * privatesign(abs(1 - VA[ii])-abs(1 - VB[ii]))* abs(abs(1 - VA[ii])-abs(1 - VB[ii])+ bias) *(1-alphaA[ii])
+                dalphaB = 0
+                alphaA[ii + 1] = dalphaA + alphaA[ii]
+                alphaA[ii + 1] = clamp(alphaA[ii + 1], 0, 1)
+                alphaB[ii + 1] = dalphaB + alphaB[ii]
+                alphaB[ii + 1] = clamp(alphaB[ii + 1], 0, 1)
 
-                dA = alphaA * betaA[ii] * (1 - VA[ii])
+                dA = betaA * alphaA[ii] * (1 - VA[ii])
                 dB = 0
                 VA[ii + 1] = dA + VA[ii]
                 VB[ii + 1] = dB + VB[ii]
 
             else:  # A, No shock trials
-                dbetaA = -1 * privatesign(abs(0 - VA[ii]) - abs(0 - VB[ii])) * abs(abs(0 - VA[ii]) - abs(0 - VB[ii])+ bias) *betaA[ii]
-                dbetaB = 0
-                betaA[ii + 1] = dbetaA + betaA[ii]
-                betaA[ii+1] = clamp(betaA[ii+1], 0, 1)
-                betaB[ii + 1] = dbetaB + betaB[ii]
-                betaB[ii + 1] = clamp(betaB[ii + 1], 0, 1)
+                dalphaA = -1 * privatesign(abs(0 - VA[ii]) - abs(0 - VB[ii])) * abs(abs(0 - VA[ii]) - abs(0 - VB[ii])+ bias) *(1-alphaA[ii])
+                dalphaB = 0
+                alphaA[ii + 1] = dalphaA + alphaA[ii]
+                alphaA[ii+1] = clamp(alphaA[ii+1], 0, 1)
+                alphaB[ii + 1] = dalphaB + alphaB[ii]
+                alphaB[ii + 1] = clamp(alphaB[ii + 1], 0, 1)
 
-                dA = alphaA * betaA[ii] * (0 - VA[ii])
+                dA = betaA * alphaA[ii] * (0 - VA[ii])
                 dB = 0
 
                 VA[ii + 1] = dA + VA[ii]
@@ -81,39 +81,39 @@ for rep in range(REP_NUM):
 
         else:  # B trials
             if trialorder[ii] >= trialn - (trialn * (1 - Aprob) * Bshock):  # No CS, yes Shock trials
-                dbetaA = 0
-                dbetaB = -1 * privatesign(abs(1 - VB[ii]) - abs(1 - VA[ii])) * abs(abs(1 - VB[ii]) - abs(1 - VA[ii])+ bias)*betaB[ii]
-                betaA[ii + 1] = dbetaA + betaA[ii]
-                betaA[ii + 1] = clamp(betaA[ii + 1], 0, 1)
-                betaB[ii + 1] = dbetaB + betaB[ii]
-                betaB[ii + 1] = clamp(betaB[ii + 1], 0, 1)
+                dalphaA = 0
+                dalphaB = -1 * privatesign(abs(1 - VB[ii]) - abs(1 - VA[ii])) * abs(abs(1 - VB[ii]) - abs(1 - VA[ii])+ bias)*(1-alphaB[ii])
+                alphaA[ii + 1] = dalphaA + alphaA[ii]
+                alphaA[ii + 1] = clamp(alphaA[ii + 1], 0, 1)
+                alphaB[ii + 1] = dalphaB + alphaB[ii]
+                alphaB[ii + 1] = clamp(alphaB[ii + 1], 0, 1)
 
                 dA = 0
-                dB = alphaB * betaB[ii] * (1 - VB[ii])
+                dB = betaB * alphaB[ii] * (1 - VB[ii])
 
                 VA[ii + 1] = dA + VA[ii]
                 VB[ii + 1] = dB + VB[ii]
 
             else: # B, no Shock
-                dbetaA = 0
-                dbetaB = -1 * privatesign(abs(0 - VB[ii]) - abs(0 - VA[ii])) * abs(abs(0 - VB[ii]) - abs(0 - VA[ii])+ bias) *betaB[ii]
-                betaA[ii + 1] = dbetaA + betaA[ii]
-                betaA[ii + 1] = clamp(betaA[ii + 1], 0, 1)
-                betaB[ii + 1] = dbetaB + betaB[ii]
-                betaB[ii + 1] = clamp(betaB[ii + 1], 0, 1)
+                dalphaA = 0
+                dalphaB = -1 * privatesign(abs(0 - VB[ii]) - abs(0 - VA[ii])) * abs(abs(0 - VB[ii]) - abs(0 - VA[ii])+ bias) *(1-alphaB[ii])
+                alphaA[ii + 1] = dalphaA + alphaA[ii]
+                alphaA[ii + 1] = clamp(alphaA[ii + 1], 0, 1)
+                alphaB[ii + 1] = dalphaB + alphaB[ii]
+                alphaB[ii + 1] = clamp(alphaB[ii + 1], 0, 1)
 
 
                 dA = 0
-                dB = alphaB * betaB[ii] * (0 - VB[ii])
+                dB = betaB * alphaB[ii] * (0 - VB[ii])
 
                 VA[ii + 1] = dA + VA[ii]
                 VB[ii + 1] = dB + VB[ii]
 
-#Todo the beta values are skyrocketing. need to set a limitation? yes: 0-1
+#Todo the alpha values are skyrocketing. need to set a limitation? yes: 0-1
     REP_A = np.vstack((REP_A, VA))
     REP_B = np.vstack((REP_B, VB))
-    REP_betaA = np.vstack((REP_betaA, betaA))
-    REP_betaB = np.vstack((REP_betaB, betaB))
+    REP_alphaA = np.vstack((REP_alphaA, alphaA))
+    REP_alphaB = np.vstack((REP_alphaB, alphaB))
 
 fig1 = plt.figure()
 fig1.clf()
@@ -131,12 +131,12 @@ plt.plot(mVB, 'b')
 fig2 = plt.figure()
 fig2.clf()
 
-mbetaA = np.mean(REP_betaA,0)
-mbetaB = np.mean(REP_betaB,0)
+malphaA = np.mean(REP_alphaA,0)
+malphaB = np.mean(REP_alphaB,0)
 
 # ax2 = fig2.subplots(1,2)
-plt.plot(mbetaA,'r')
-# ax2[0].set_title('$mean beta_{A}$')
-plt.plot(mbetaB,'b')
-# ax2[1].set_title('$mean beta_{B}$')
+plt.plot(malphaA,'r')
+# ax2[0].set_title('$mean alpha_{A}$')
+plt.plot(malphaB,'b')
+# ax2[1].set_title('$mean alpha_{B}$')
 
