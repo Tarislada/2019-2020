2dc3d654c7606899bfb5e54b805ebcf7743573b1
Tarislada <57513585+Tarislada@users.noreply.github.com>
2019-11-23 21:14:47 +0900

    Update Assignmnet3b.m

--- MATLAB/Brainsignals/Assignmnet3b.m
+++ MATLAB/Brainsignals/Assignmnet3b.m
@@ -1,10 +1,11 @@
+%% Create DoMiSolChord
 [pfy,pffs] = audioread('DoMiSolDo-Pf.mp3');
-[vny,vnfs] = audioread('DoMiSolDo-Vn.mp3');
-pf1 = abs(fft(pfy(:,1)));
-pf2 = abs(fft(pfy(:,2)));
-vn1 = fft(vny(:,1));
-vn2 = fft(vny(:,2));
+pfy = mean(pfy,2);
+for ii = 1:3
+    Domi(ii,:) = pfy(1+88200*(ii-1):1+88200*(ii));
+end
+DoMiSol = sum(Domi,1);
+%% 
+
+pf1 = fftshift(abs(fft(DoMiSol)));
 fpf1 = (0:length(pf1)-1)*50/length(pf1);
-fpf2 = (0:length(pf2)-1)*50/length(pf2);
-fvn1 = (0:length(vn1)-1)*50/length(vn1);
-fvn2 = (0:length(vn2)-1)*50/length(vn2);
