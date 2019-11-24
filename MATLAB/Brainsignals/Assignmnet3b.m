598871c8601db2ac6060459afdc9def802b064c8
Tarislada <57513585+Tarislada@users.noreply.github.com>
2019-11-24 22:18:22 +0900

    assignment3 - a completed
    finished 3a using pitch from 2019b addon

--- MATLAB/Brainsignals/Assignmnet3b.m
+++ MATLAB/Brainsignals/Assignmnet3b.m
@@ -9,16 +9,21 @@ Chord = sum(DoMiSol,1);
 figure(1)
 Do_x = (0:length(DoMiSol(1,:))-1)*50/length(DoMiSol(1,:));
 plot(Do_x,DoMiSol(1,:))
-dof0= mean(pitch(DoMiSol(1,:)',44100));
+Do_fund = mean(pitch(DoMiSol(1,:)',44100));
 
 figure(2)
 Mi_x = (0:length(DoMiSol(2,:))-1)*50/length(DoMiSol(2,:));
 plot(Mi_x,DoMiSol(2,:))
+Mi_fund = mean(pitch(DoMiSol(2,:)',44100));
 
 figure(3)
 Sol_x = (0:length(DoMiSol(3,:))-1)*50/length(DoMiSol(3,:));
 plot(Sol_x,DoMiSol(3,:))
+Sol_fund = mean(pitch(DoMiSol(3,:)',44100));
 
 figure(4)
 Chord_x = (0:length(Chord)-1)*50/length(Chord);
 plot(Chord_x,Chord)
+Chord_fund = mean(pitch(Chord',44100));
+
+%% Assignment3(b)
