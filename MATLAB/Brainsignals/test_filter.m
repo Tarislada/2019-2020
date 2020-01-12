ab5c7b70702d0da28037ea3238de8e48351071a0
Tarislada <57513585+Tarislada@users.noreply.github.com>
2020-01-12 15:35:56 +0900

    Assignment completed

--- MATLAB/Brainsignals/test_filter.m
+++ MATLAB/Brainsignals/test_filter.m
@@ -1,5 +1,5 @@
 [chord_sample1,fs_cs]=audioread('DoMiSolChord.mp3');
 h = spectrum.welch;
 
-y1 = filter(dofilttest,1,chord_sample1);
-audiowrite('dofilttest.wav',y1*20,fs_cs);
+y1 = filter(dofiltertest,1,chord_sample1);
+audiowrite('dofiltertest.wav',y1*15,fs_cs);
